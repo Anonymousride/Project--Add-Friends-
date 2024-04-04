094d16537d1997726b60cbcdcbbf9a351849df4a
Manish Kumar <90982862+Anonymousride@users.noreply.github.com>
2024-04-04 20:40:28 +0530

    Add files via upload

---
+++ project14(Make friends)/script.js
@@ -0,0 +1,245 @@
+let user = [
+    {profilePic: "Ravan.webp", 
+    displayPic: "manish pic.jpeg",
+    pendingMessage: 1,
+    location: "Meerut,India", 
+    name: "Manish", 
+    age:21, 
+    interests: [{
+        icons: `<i class="ri-football-fill"></i>`,
+        interest: "Volleyball"
+    },  
+    {   icons: `<i class="ri-music-2-fill"></i>`,
+        interest: "Music"
+    }], 
+    Bio: "Lorem ipsum dolor, sit amet consectetur aaadipisicing elit. Esse libero veniam corrupti praesentium, voluptatibus dignissimos.",
+    isfriend: null
+    },
+    {profilePic: "https://images.unsplash.com/photo-1581391528803-54be77ce23e3?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", 
+    displayPic: "https://images.unsplash.com/photo-1473830394358-91588751b241?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    pendingMessage: 2,
+    location: "Delhi,India", 
+    name: "Rishabh", 
+    age:25, 
+    interests: [{
+        icons: `<i class="ri-football-fill"></i>`,
+        interest: "Cricket"
+    },  
+    {   icons: `<i class="ri-music-2-fill"></i>`,
+        interest: "Volleyball"
+    }], 
+    Bio: "Lorem ipsum dolor,  adipisicing elit. Esse libero veniam corrupti praesentium, voluptatibus dignissimos.",
+    isfriend: null
+    },
+    {profilePic: "https://plus.unsplash.com/premium_photo-1688891564708-9b2247085923?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", 
+    displayPic: "https://images.unsplash.com/photo-1496302662116-35cc4f36df92?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    pendingMessage: 3,
+    location: "Saharanpur,India", 
+    name: "Rajat", 
+    age:29, 
+    interests: [{
+        icons: `<i class="ri-football-fill"></i>`,
+        interest: "Volleyball"
+    },  
+    {   icons: `<i class="ri-music-2-fill"></i>`,
+        interest: "Music"
+    }], 
+    Bio: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Esse libero corrupti praesentium, voluptatibus dignissimos.",
+    isfriend: null
+    },
+    {profilePic: "https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?q=80&w=1965&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    displayPic: "https://images.unsplash.com/photo-1503249023995-51b0f3778ccf?q=80&w=1860&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", 
+    pendingMessage: 4,
+    location: "Modinagar,India", 
+    name: "Akshit", 
+    age:22, 
+    interests: [{
+        icons: `<i class="ri-football-fill"></i>`,
+        interest: "Volleyball"
+    },  
+    {   icons: `<i class="ri-music-2-fill"></i>`,
+        interest: "Music"
+    }], 
+    Bio: "Lorem ipsum dolor, sit amet consectetur. Esse libero veniam corrupti praesentium, dignissimos.",
+    isfriend: null
+    },
+    {profilePic: "https://plus.unsplash.com/premium_photo-1695922660057-1358ce8dc023?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", 
+    displayPic: "https://images.unsplash.com/photo-1575844261401-d69721eb5044?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    pendingMessage: 642,
+    location: "Death,Hell", 
+    name: "Devil", 
+    age:238003, 
+    interests: [{
+        icons: `<i class="ri-football-fill"></i>`,
+        interest: "Fire"
+    },  
+    {   icons: `<i class="ri-music-2-fill"></i>`,
+        interest: "Laugh"
+    }], 
+    Bio: "Lorem ipsum dolor,  adipisicing elit. Esse libero veniam corrupti praesentium, voluptatibus dignissimos.",
+    isfriend: null
+    },
+    {profilePic: "https://images.unsplash.com/photo-1466112928291-0903b80a9466?q=80&w=1773&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", 
+    displayPic: "https://images.unsplash.com/photo-1580321187070-da8bdee36013?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    pendingMessage: 12,
+    location: "undefine,Illuminati", 
+    name: "Anonymous", 
+    age:20, 
+    interests: [{
+        icons: `<i class="ri-football-fill"></i>`,
+        interest: "Hacking"
+    },  
+    {   icons: `<i class="ri-music-2-fill"></i>`,
+        interest: "Bugs bounting"
+    }], 
+    Bio: "Lorem ipsum dolor,  adipisicing elit. Esse libero veniam corrupti praesentium, voluptatibus dignissimos.",
+    isfriend: null
+    },
+    {profilePic: "https://images.unsplash.com/photo-1659095332109-16093b523cd2?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", 
+    displayPic: "https://images.unsplash.com/photo-1580059887244-67ae60a8f432?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    pendingMessage: 13,
+    location: "Bengal,India", 
+    name: "Raaj", 
+    age:28, 
+    interests: [{
+        icons: `<i class="ri-football-fill"></i>`,
+        interest: "Black magic"
+    },  
+    {   icons: `<i class="ri-music-2-fill"></i>`,
+        interest: "Nail"
+    }], 
+    Bio: "Lorem ipsum dolor,  adipisicing elit. Esse libero veniam corrupti praesentium, voluptatibus dignissimos.",
+    isfriend: null
+    },
+    {profilePic: "https://images.unsplash.com/photo-1696789738783-0f972304597e?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", 
+    displayPic: "https://images.unsplash.com/photo-1529946825183-536c6317f60e?q=80&w=1963&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    pendingMessage: 21,
+    location: "Shadow,Error", 
+    name: "Unknown", 
+    age:21, 
+    interests: [{
+        icons: `<i class="ri-football-fill"></i>`,
+        interest: "Cricket"
+    },  
+    {   icons: `<i class="ri-music-2-fill"></i>`,
+        interest: "Hack"
+    }], 
+    Bio: "Lorem ipsum dolor,  adipisicing elit. Esse libero veniam corrupti praesentium, voluptatibus dignissimos.",
+    isfriend: null
+    },
+];
+
+function select(elem){
+    return document.querySelector(elem);
+}
+let curr = 0;
+let isAnimating = false;
+
+function setData(index){
+    select(".prfimg img").src = user[index].profilePic;
+    select(".badge h5").textContent = user[index].pendingMessage;
+    select(".location h3").textContent = user[index].location;
+    select(".name h1:nth-child(1)").textContent = user[index].name;
+    select(".name h1:nth-child(2)").textContent = user[index].age;
+
+    let clutter = "";
+    user[curr].interests.forEach(function(interest){
+        clutter += ` <div class="tag flex items-center text-white bg-white/30 px-3 py-1 rounded-full gap-3">
+        ${interest.icons}
+        <h3 class="text-sm tracking-tight capitalize">${interest.interest}</h3>
+    </div>`
+    })
+    select(".tags").innerHTML = clutter;
+
+    select(".bio p").textContent = user[curr].Bio;
+}
+(function setInitial(){
+    select(".maincard img").src = user[curr].displayPic;
+    select(".insidecard img").src = user[curr+1]?.displayPic;
+
+    setData(curr);
+   
+    curr = 2;
+})();
+
+function imageChange(){
+    if(!isAnimating){
+        isAnimating = true;
+        let t1 = gsap.timeline({
+            onComplete: function(){
+             isAnimating = false;
+             let main = select(".maincard");
+             let inside = select(".insidecard");
+     
+             inside.classList.remove("z-[2]");
+             inside.classList.add("z-[3]");
+             inside.classList.remove("insidecard");
+     
+             main.classList.remove("z-[3]");
+             main.classList.add("z-[2]");
+             gsap.set(main, {
+                 scale: 1,
+                 opacity: 1
+             })
+             if(curr === user.length) curr = 0;
+             select(".maincard img").src = user[curr].displayPic;
+             curr++;
+             main.classList.remove("maincard");
+             inside.classList.add("maincard");
+             main.classList.add("insidecard");
+            }
+        });
+        t1.to(".maincard", {
+         scale: 1.1,
+         opacity: 0,
+         ease: Circ,
+         duration: .9
+        }, "a")
+     
+        t1.from(".insidecard", {
+         scale: .9,
+         opacity: 0,
+         ease: Circ,
+         duration: 1.1
+        }, "a")
+    }
+}
+
+    let deny = select(".deny");
+    let accept = select(".accept");
+
+    deny.addEventListener("click", function(){
+      imageChange();
+      setData(curr-1);
+      gsap.from(".details .element", {
+        y: "100%",
+        opacity: 0,
+        stagger: .06,
+        ease: Power4.easeInOut,
+        duration: 1.5
+      })
+    });
+   
+    accept.addEventListener("click", function(){
+    imageChange();
+    setData(curr-1);
+    gsap.from(".details .element", {
+      y: "100%",
+      opacity: 0,
+      stagger: .06,
+      ease: Power4.easeInOut,
+      duration: 1.5
+    })
+    });
+
+    (function containerCreator(){
+    document.querySelectorAll(".element")
+    .forEach(function (element) {
+       let div = document.createElement("div");
+       div.classList.add(`${element.classList[1]}container`, 'overflow-hidden');
+       div.appendChild(element);
+       select(".details").appendChild(div);
+    })
+    })();
+
+   
